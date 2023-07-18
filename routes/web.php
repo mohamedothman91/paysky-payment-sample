1367b47a0d98d77e6695f742e382efe4d823e81c
mohamed-othman <mohamed.othman@vodafone.com>
2023-07-18 10:50:56 +0300

    Init Commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+use Illuminate\Http\Request;
+
+
+Route::get('/', function () {
+  
+    $payment = PaySky::makePayment(100000 , rand());
+    return view('welcome' ,  ['payment' => $payment ]  );
+});
+
+
+Route::get('/completeCallback', function (Request $request) {
+  
+    dd($request->all());
+})->name('completeCallback');;
+
+
+Route::get('/errorCallback', function (Request $request) {
+  
+    dd($request->all());
+})->name('errorCallback');
+
